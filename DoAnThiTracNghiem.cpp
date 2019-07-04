200c3665b5a26ca0bb528b1257b4a0419e62df65
SouthBlue <giangnamstyle1998@gmail.com>
2019-07-04 21:55:43 +0700

    Initial commit

---
+++ DoAnThiTracNghiem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <mylib.h>
+#include <windows.h>
+using namespace std;
+
+
+
+
+void resizeConsole()
+{
+	HWND console = GetConsoleWindow();
+	SetWindowPos( console, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER );
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, 1365, 730, TRUE);
+}
+
+
+
+
+int main(){
+	resizeConsole();
+}
