858f5af5338aaf362367e69e5a5b9d1387518321
Duongdev97 <hongduong7742@gmail.com>
2019-07-09 11:36:43 +0700

    new
    Ten bien moi + sua mot so khai bao

---
+++ lib.h
@@ -0,0 +1,24 @@
+void rectangle(int x, int y, int width, int height)
+{
+	gotoxy(x, y);
+	cout << static_cast<char>(201);
+	for (int i = x; i < x + width - 2; i++) {
+		gotoxy(i + 1, y);
+		cout << static_cast<char>(205);
+		gotoxy(i + 1, y + height - 1);
+		cout << static_cast<char>(205);
+	}
+	gotoxy(x + width - 1, y);
+	cout << static_cast<char>(187) << endl;
+	for (int i = y; i < y + height - 1; i++) {
+		gotoxy(x, i + 1);
+		cout << static_cast<char>(186);
+		gotoxy(x + width - 1, i + 1);
+		cout << static_cast<char>(186) << endl;
+	}
+	gotoxy(x, y + height - 1);
+	cout << static_cast<char>(200);
+	gotoxy(x + width - 1, y + height - 1);
+	cout << static_cast<char>(188);
+
+}
